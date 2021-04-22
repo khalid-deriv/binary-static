0deb71b3bc1eb42a49dcbdd64b980efba03a65fe
khalidxx <fxtryio@gmail.com>
2021-04-22 16:47:22 +0800

    blocked access from footer

--- src/javascript/app/base/binary_loader.js
+++ src/javascript/app/base/binary_loader.js
@@ -108,6 +108,7 @@ const BinaryLoader = (() => {
         only_real        : () => localize('This feature is not relevant to virtual-money accounts.'),
         not_authenticated: () => localize('This page is only available to logged out clients.'),
         no_mf            : () => localize('Sorry, but binary options trading is not available in your financial account.'),
+        options_blocked  : () => localize('Sorry, but binary options trading is not available in your country.'),
     };
 
     const loadHandler = (this_page) => {
@@ -141,6 +142,9 @@ const BinaryLoader = (() => {
         }
         if (config.no_mf && Client.isLoggedIn() && Client.isAccountOfType('financial')) {
             BinarySocket.wait('authorize').then(() => displayMessage(error_messages.no_mf()));
+        }
+        if (config.no_blocked_country && Client.isLoggedIn() && Client.isOptionsBlocked()) {
+            BinarySocket.wait('authorize').then(() => displayMessage(error_messages.options_blocked()));
         }
 
         BinarySocket.setOnDisconnect(active_script.onDisconnect);
