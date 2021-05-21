e6bb258260c3db585c87c9afe864021318ba7e8a
khalidxx <fxtryio@gmail.com>
2021-05-21 19:22:30 +0800

    added affiliate token to banner link

--- src/templates/_common/components/deriv_banner.jsx
+++ src/templates/_common/components/deriv_banner.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const DerivBanner = ({ has_margin }) => (
     <React.Fragment>
         <div id='deriv_banner_container' className={`invisible ui-helper-clearfix ${has_margin ? 'has_margin' : ''}`}>
-            <a target='_blank' rel='noopener noreferrer' href={`${it.deriv_banner_url}interim/faq/?utm_source=binary&utm_medium=referral&utm_campaign=ww-banner-deriv-1020-en&utm_content=deriv-banner-rebranding`}>
+            <a target='_blank' rel='noopener noreferrer' href={`${it.deriv_banner_url}interim/faq/?t=${it.affiliate_token}&utm_source=binary&utm_medium=referral&utm_campaign=ww-banner-deriv-1020-en&utm_content=deriv-banner-rebranding`}>
                 <img className='deriv_banner_background' src={it.url_for('images/deriv/deriv-banner-bg.png')} />
                 <img className='deriv_banner_background deriv_banner_background_mobile' src={it.url_for('images/deriv/deriv-banner-bg-mobile.png')} />
                 <img className='deriv_banner_chevron' src={it.url_for('images/deriv/chevron_right.svg')} />
@@ -15,7 +15,7 @@ const DerivBanner = ({ has_margin }) => (
             <div className='deriv_banner_close' />
         </div>
         <div id='multiplier_banner_container' className={`invisible ui-helper-clearfix ${has_margin ? 'has_margin' : ''}`}>
-            <a target='_blank' rel='noopener noreferrer' href={`${it.deriv_banner_url}trade-types/multiplier/?utm_source=binary&utm_medium=referral&utm_campaign=ww-banner-deriv-1020-en&utm_content=multiplier-banner-synthetic-indices-amplified`}>
+            <a target='_blank' rel='noopener noreferrer' href={`${it.deriv_banner_url}trade-types/multiplier/?t=${it.affiliate_token}&utm_source=binary&utm_medium=referral&utm_campaign=ww-banner-deriv-1020-en&utm_content=multiplier-banner-synthetic-indices-amplified`}>
                 <div className='multiplier_banner_title'>
                     <img className='multiplier_banner_logo' src={it.url_for('images/deriv/deriv-logo.svg')} />
                     <h3>{it.L('Multipliers')}</h3>
