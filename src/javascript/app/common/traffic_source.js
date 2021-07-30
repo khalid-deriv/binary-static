107b4394fb3cd127de79455c298266b9d90db60e
khalidxx <fxtryio@gmail.com>
2021-07-30 17:14:07 +0800

    adding utm ags based on priority

--- src/javascript/app/common/traffic_source.js
+++ src/javascript/app/common/traffic_source.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const Client         = require('../base/client');
 const CookieStorage  = require('../../_common/storage').CookieStorage;
 const LocalStore     = require('../../_common/storage').LocalStore;
@@ -15,6 +16,16 @@ const Url            = require('../../_common/url');
 
 const TrafficSource = (() => {
     let cookie;
+    const utm_fields = [
+        'utm_source',
+        'utm_medium',
+        'utm_campaign',
+        'utm_term',
+        'utm_content',
+        'utm_ad_id',
+        'utm_adgroup_id',
+        'utm_campaign_id',
+    ];
 
     const initCookie = () => {
         if (!cookie) {
@@ -35,8 +46,28 @@ const TrafficSource = (() => {
         return data;
     };
 
+    const shouldOverwrite = (new_utm_data, current_utm_data) => {
+        if (!current_utm_data) {
+            return true;
+        } else if (!new_utm_data) {
+            return false;
+        }
+        
+        // Check if both new and old utm_data has all required filds
+        const required_fields = ['utm_source', 'utm_medium', 'utm_campaign'];
+        const has_all_params = required_fields.every((field) => new_utm_data[field] && current_utm_data[field]);
+        
+        // Overwrite based on the order of priority
+        if (has_all_params) {
+            if (new_utm_data.utm_medium.includes('aff')) return true; // 1. Affiliate tags
+            else if (new_utm_data.utm_medium.includes('ppc') && !current_utm_data.utm_medium.includes('aff')) return true; // 2. PPC tags
+            else if (!current_utm_data.utm_medium.includes('ppc') && !current_utm_data.utm_medium.includes('aff')) return true; // 3. Complete set of required tags
+        } else if (Object.values(new_utm_data).length > Object.values(current_utm_data).length) return true; // 4. Everything else
+        return false;
+    };
+
     // get source in order of precedence
-    const getSource = (utm_data = getData()) => utm_data.utm_source || utm_data.referrer || 'direct';
+    const getSource = (utm_data = getData()) => utm_data.utm_source || utm_data.referrer || null;
 
     const setData = () => {
         if (Client.isLoggedIn()) {
@@ -44,26 +75,25 @@ const TrafficSource = (() => {
             return;
         }
 
+        const new_values     = { utm_source: getSource() };
         const current_values = getData();
         const params         = Url.paramsHash();
-        const param_keys     = ['utm_source', 'utm_medium', 'utm_campaign'];
 
-        // When the user comes to the site with URL params
-        if (params.utm_source || params.utm_medium || params.utm_campaign) {
-
-            // if url is missing one of required fields, do nothing
-            const has_all_params = param_keys.every((param) => param in params);
-            
-            if (has_all_params) {
-                param_keys.forEach((key) => {
-                    if (params[key]) {
-                        cookie.set(key, params[key], { sameSite: 'none', secure: true });
-                    }
-                });
+        // If the user has any new UTM params, store them
+        utm_fields.forEach((field) => {
+            if (params[field]) {
+                new_values[field] = params[field].replace(/[^a-zA-Z0-9\s\-\.\_]/gi, '').substring(0, 100); // Limit to 100 supported characters
             }
-            
-        } else if (!current_values.utm_source) {
-            cookie.set('utm_source', 'binary_direct', { sameSite: 'none', secure: true });
+        });
+
+        // Check if params has utm data
+        if (shouldOverwrite(new_values, current_values)) {
+            clearData();
+            Object.keys(new_values).forEach((key) => {
+                if (new_values[key]) {
+                    cookie.set(key, new_values[key], { sameSite: 'none', secure: true });
+                }
+            });
         }
 
         // Store gclid
